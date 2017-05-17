f9596ee0f8556aac552a867f5d5571ca5243c88c
Steve Larsen <stevelarsen01@gmail.com>
2017-05-17 18:14:31 -0500

    listening for actions - Tutorial 10

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,9 +1,11 @@
 /*jshint esversion: 6 */
 import {combineReducers} from 'redux';
 import UserReducer from './reducer-users';
+import ActiveUserReducer from './reducer-active-user';
 
 const allReducers = combineReducers({
-  users: UserReducer
+  users: UserReducer,
+  activeUser: ActiveUserReducer
 });
 
 export default allReducers;
