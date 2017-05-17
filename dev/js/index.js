936b9a6bc1d45a20b785c72bc630d14b6e2a5e9d
Steve Larsen <stevelarsen01@gmail.com>
2017-05-17 17:16:06 -0500

    mapStateToProps - Tutorial 7

--- dev/js/index.js
+++ dev/js/index.js
@@ -11,4 +11,8 @@ const store = createStore(allReducers);
 
 // console.log(store);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
